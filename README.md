f87bb4218b4f1e7ee5a5cd16fb59c368783d2991
Junior Alves <jrnalves@gmail.com>
2023-03-28 12:47:00 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Framer motion landin page
+### 🚀 [landing page com framer motion](https://junioralvesbr.github.io/landingpage-fitness-motion/)
+
+![Alt Text](/public/landing.png)
+
+![HTML](https://img.shields.io/badge/HTML5-orange) ![CSS](https://img.shields.io/badge/CSS3-blue) ![Javascript](https://img.shields.io/badge/JavaScript-yellow) ![React](https://img.shields.io/badge/React-%2361dafb) ![Motion](https://img.shields.io/badge/Framer--Motion-%23d2c)
+#
+## 📋 Descrição: 
+ Esse site foi criado para treinar a biblioteca frame motion. E desenvolver habilidade de animação de sites.
+#
+
+## 🔧 Descrição técnica:
+ Nesse projeto foi usado:
+ - ReactJS com Vite, sass, framer-motion.
+ - O site esta hospedado no github utilizando deploy em CI/CD
+#
+
+## 📄 Aprendizado:
+Neste projeto tive vários desafios, entre eles destaco:
+ - A criação do layout buscando vários sites como refêrencia
+ - Realizar a animação dos componentes com framer-motion.
+
+#
+## Autor: Junior Alves
+[![Linkedin](https://img.shields.io/badge/Linkedin-Junior%20Alves-blue)](https://www.linkedin.com/in/junior-alves-54559070/)
+[![Instagram](https://img.shields.io/badge/Instagram-%40junioralvesbr4-%23e4405f)](https://www.instagram.com/junioralvesbr4/)
+![gmail](https://img.shields.io/badge/Gmail-jrnalves%40gmail.com-red)
---
+++ src/App.jsx
@@ -0,0 +1,181 @@
+import { motion } from 'framer-motion'
+import './App.scss'
+
+const stagger = {
+  animate: {
+    transition: {
+      delayChildren: 0.8,
+      staggerChildren: 0.2
+    }
+  }
+}
+
+const header = {
+  initial: {
+    y: -60,
+    opacity: 0,
+    transition: {
+      duration: 0.05,
+      ease: "easeInOut"
+    }
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: "easeInOut"
+    }
+  }
+}
+
+const items = {
+  initial: {
+    x: -60,
+    opacity: 0
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: "easeInOut"
+    }
+  }
+}
+
+const item_right = {
+  initial: {
+    y: 60,
+    opacity: 1
+  },
+  animate: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.6,
+      ease: "easeInOut"
+    }
+  }
+}
+
+function App() {
+  return (
+    <motion.div
+      className="container"
+      initial={{ opacity: 0, height: 0 }}
+      animate={{ opacity: 1, height: "100vh" }}
+      transition={{ duration: 1, ease: "easeInOut" }}
+    >
+      <motion.div
+        className='circle'
+        initial={{ y: "-100%", x: "-50%", opacity: 0 }}
+        animate={{ y: "-50%", x: "-50%", opacity: 1 }}
+        transition={{ duration: 1, delay: 1, type: 'spring', stiffness: 500 }}
+      ></motion.div>
+      <div className='model'>
+        <motion.img
+          src="/model.png"
+          alt="Modelo fitness"
+          initial={{ x: -60, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, delay: 1.5, type: "spring", stiffness: 500 }}
+        />
+      </div>
+
+      <motion.div className='wrapper' initial="initial" animate="animate">
+        <motion.header variants={stagger}>
+          <motion.div className='menu_container' variants={header}>
+            <div className='menu'>
+              <i className='fa fa-bars'></i>
+              <span>Menu</span>
+            </div>
+            <div className='search'>
+              <i className='fa fa-search'></i>
+              <span>Search</span>
+            </div>
+          </motion.div>
+
+          <motion.div className='logo-container' variants={header}>
+            <span>D</span>
+            <span>A</span>
+          </motion.div>
+
+          <motion.div className='cart_container' variants={header}>
+            <div className='cart'>
+              <span>Cart</span>
+              <i className='fa fa-cart-arrow-down'></i>
+            </div>
+
+            <div className='user'>
+              <span>Login</span>
+              <i className='fa fa-user'></i>
+            </div>
+          </motion.div>
+        </motion.header>
+
+        <motion.div className='content_wrapper' initial="initial" animate="animate">
+          <motion.div className='item_left' variants={stagger}>
+            <motion.ul className='info'>
+              <motion.li variants={items}>Cyclists</motion.li>
+              <motion.li variants={items}>Runners</motion.li>
+              <motion.li variants={items}>Swimmers</motion.li>
+              <motion.li variants={items}>Boxer</motion.li>
+            </motion.ul>
+
+            <motion.div className='main_title'>
+              <motion.p
+                initial={{ opacity: 0, x: -20 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: .3, delay: 1.7, type: "spring", stiffness: 500 }}
+              >1.<span>We create for you</span></motion.p>
+              <motion.h2
+                initial={{ opacity: 0, x: -80 }}
+                animate={{ opacity: 1, x: -10 }}
+                transition={{ duration: .6, delay: 2, type: "spring", stiffness: 500 }}
+              >
+                Create
+              </motion.h2>
+            </motion.div>
+
+            <motion.div className='message' variants={items}>
+              Lorem, ipsum dolor sit amet <br /> consectetur adipisicing elit.
+            </motion.div>
+
+            <motion.div className='btn_group' variants={stagger}>
+              <motion.div className='btn primary' variants={items}>
+                <span>Open Shop</span>
+              </motion.div>
+
+              <motion.div className='btn secondary' variants={items} animate={{ skew: "-21deg" }}>
+                <span>Open Shop</span>
+              </motion.div>
+            </motion.div>
+          </motion.div>
+
+          <motion.div className='item_right' variants={stagger}>
+            <motion.p variants={item_right}>product <span>02</span></motion.p>
+            <motion.h2 variants={item_right}>way the <br />right way</motion.h2>
+
+            <motion.div className='message' variants={item_right}>
+              Lorem ipsum dolor sit
+              <br />amet consectetur
+              <br />adipisicing elit.
+            </motion.div>
+
+            <motion.nav className='nav' variants={item_right}>
+              <span>
+                <i className='fa fa-chevron-left'></i>
+              </span>
+              <span className='active'>
+                <i className='fa fa-chevron-right'></i>
+              </span>
+            </motion.nav>
+          </motion.div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
+  )
+}
+
+export default App
